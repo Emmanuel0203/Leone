aed4b59941a069fd4a7f2c7618facc8760f8e61a
EMMANUEL ALVAREZ QUINTERO <alvarezemmanuel0203@gmail.com>
2024-02-07 08:14:34 -0500

    Primer sesion-Creacion Fundamentos

---
+++ proyecto/Leone/LeoneDS/dbo/Tables/Pais.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Pais]
+(
+	[IdPais] UNIQUEIDENTIFIER NOT NULL  DEFAULT NewId(), 
+    [Nombre] NVARCHAR(MAX) NULL, 
+    [Activo] BIT NULL, 
+    [Actualiza] DATETIME NULL DEFAULT GetDate(), 
+    CONSTRAINT [PK_Pais] PRIMARY KEY ([IdPais])
+)
